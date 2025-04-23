cb7815a2c2367c35e842d0ca9c57c8a634d47455
braian <braiancardona1245@gmail.com>
2025-04-23 08:59:05 -0500

    aprender

---
+++ 1 al 100.py
@@ -0,0 +1,28 @@
+for numero in range(1, 101):
+    if numero % 3 == 0:
+        print("Fizz")
+    else:
+        print(numero)
+
+
+for numero in range(1, 101):
+    if numero % 5 == 0:
+        print("Buzz")
+    else:
+        print(numero)
+
+for numero in range(1, 101):
+    if numero % 3 == 0 and numero % 5 == 0:
+        print("FizzBuzz")
+    else:
+        print(numero)
+
+
+
+
+
+
+
+
+
+
