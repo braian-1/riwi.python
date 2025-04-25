4ab5ef6319d040a1e50c627ff37d1b8f0799c410
braian <braiancardona1245@gmail.com>
2025-04-25 11:08:07 -0500

    arregl

--- tien.py
+++ tien.py
@@ -14,13 +14,15 @@ total = precio * cantidad
 print("El total de la compra es: ")
 print(total)
 
-if total >= 250000:
+if total > 250000:
+#si el valor es mayor a 250000 aplica para un descuento del 10%
     print("tienes un descuento del 10% ")
     if total >= 250000 : descuento=(total * 0.1)
     total_con_descuento= total - descuento
     print("el total de la compra con descuento es: ")
     (input(total_con_descuento))
     print("precio con descuento: ", total_con_descuento)
+#si el valor es menor o igual a 250000 no cumple para el descuento
 else:
     print("no cumples para el descuento. ")
 
