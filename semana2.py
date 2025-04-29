fb15883a9676bcaad05a5e46720d6530dd086d38
braian <braiancardona1245@gmail.com>
2025-04-29 10:41:04 -0500

    semana2

--- semana2.py
+++ semana2.py
@@ -1,34 +1,60 @@
-promedio=int(input("ingrese una calificacion de 0 a 100: "))
 
-if promedio > 70:
-    print("has aprobado")
-else:
-    print("has reprobado")
+option = input ("""Ingrese una opcion:
+1.Revision de una nota
+2.Promedio
+3.Validar nota mayor
+4.Verificar y conteo\n
+""")
+
+if int(option) == 1:
+
+    nota = int(input("Ingrese una nota: "))
+    if nota > 70:
+        print("Gano la nota")
+    else:
+        print("Perdio la nota")
+
+elif int(option) == 2:
+    lista = []
+    sumador = 0
+    promedio = 0
+    cantNote = input("Ingrese la cantidad de notas: ")
+    for i in range(1,int(cantNote)+1):
+        nota = input(f"Ingrese una nota {i}:")
+        lista.insert(i,nota)
+        sumador+=int(nota)
+        
+    promedio = sumador / int(cantNote)
+    print("El promedio de las notas es: ",promedio)
+    print(f"Las notas son {lista}")
+    
+elif int(option) == 3:
+
+    
+    notas = (print("ingresa las notas: "))
+    nota1=int(input("ingrese la primera nota: "))
+    nota2=int(input("ingrese la segunda nota: "))
+    nota3=int(input("ingrese la tercer nota: "))                
+    numero_para_comparar = int(input(" ingrese valor a comparar: "))
+    
+    contador = 0 
+    notas = [nota1, nota2, nota3]
+    print(f"comparando con: {numero_para_comparar}")
+
+    for nota in notas:
+        if nota > numero_para_comparar:
+            contador += 1
+    print(f"La cantidad de notas mayores que {numero_para_comparar}: {contador}")
 
 
-Calificaciones = input("ingresa las calificaciones separada por comas: ")
-notas = Calificaciones.split(',')
-notas = [float(num)for num in notas]
-resultado=sum(notas)
 
-print(f"la suma de las calificaciones es: {resultado}")
+elif int(option) == 4:
+    acumulador2 = 0
 
-resultado = float(input("ingresa el total de la suma: "))
-cantidad_de_numeros=int(input("cuantos numeros sumaste: "))
-promedio = resultado / cantidad_de_numeros
-
-print(f"el promedio es: {promedio}")
-
-
-lista = input("esta es la lista de calificaciones.")
-print(Calificaciones)
-
-valor=(input("ingresa un valor de las calificaciones ingresadas anteriormente:"))
-nota = 95
-
-mayores=[nota for nota in Calificaciones if valor > nota]
-print(f"hay {len (mayores)} calificaciones mayores que {nota}.")
-
-
-
-
+    nota = input("Ingrese las notas separadas por coma: ").split(",")
+    notas = list(map(int, nota))
+    for i in notas:
+        acumulador2 += i
+        promedio = acumulador2/len(notas)
+    print(f"Las notas son: {notas}")
+    print("El promedio es: ", promedio)
