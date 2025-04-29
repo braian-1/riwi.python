f6b1ec1b048f8cbec0db86de177b94c3ddc47c3a
braian <braiancardona1245@gmail.com>
2025-04-29 13:34:29 -0500

    arreglo semana2

--- semana2.py
+++ semana2.py
@@ -1,60 +1,60 @@
 
-option = input ("""Ingrese una opcion:
-1.Revision de una nota
-2.Promedio
-3.Validar nota mayor
-4.Verificar y conteo\n
+option = input ("""INGRESE UNA OPCION:
+1.REVISION DE UNA NOTA
+2.PROMEDIO
+3.VALIDAR NOTA MAYOR
+4.VERIFICAR Y CONTEO\n
 """)
 
 if int(option) == 1:
 
-    nota = int(input("Ingrese una nota: "))
+    nota = int(input("INGRESE UNA NOTA: "))
     if nota > 70:
-        print("Gano la nota")
+        print("GANO LA NOTA")
     else:
-        print("Perdio la nota")
+        print("PERDIO LA NOTA")
 
 elif int(option) == 2:
     lista = []
     sumador = 0
     promedio = 0
-    cantNote = input("Ingrese la cantidad de notas: ")
+    cantNote = input("INGRESE LA CANTIDAD DE NOTAS: ")
     for i in range(1,int(cantNote)+1):
-        nota = input(f"Ingrese una nota {i}:")
+        nota = input(f"INGRESE UNA NOTA {i}:")
         lista.insert(i,nota)
         sumador+=int(nota)
         
     promedio = sumador / int(cantNote)
-    print("El promedio de las notas es: ",promedio)
-    print(f"Las notas son {lista}")
+    print("EL PROMEDIO DE LAS NOTAS ES: ",promedio)
+    print(f"LAS NOTAS SON: {lista}")
     
 elif int(option) == 3:
 
     
-    notas = (print("ingresa las notas: "))
-    nota1=int(input("ingrese la primera nota: "))
-    nota2=int(input("ingrese la segunda nota: "))
-    nota3=int(input("ingrese la tercer nota: "))                
-    numero_para_comparar = int(input(" ingrese valor a comparar: "))
+    notas = (print("INGRESE LAS NOTAS: "))
+    nota1=int(input("INGRESE LA PRIMERA NOTA: "))
+    nota2=int(input("INGRESE LA SEGUNDA NOTA: "))
+    nota3=int(input("INGRESA LA TERCERA NOTA: "))                
+    numero_para_comparar = int(input(" INGRESE VALOR A COMPARAR: "))
     
     contador = 0 
     notas = [nota1, nota2, nota3]
-    print(f"comparando con: {numero_para_comparar}")
+    print(f"COMPARANDO CON: {numero_para_comparar}")
 
     for nota in notas:
         if nota > numero_para_comparar:
             contador += 1
-    print(f"La cantidad de notas mayores que {numero_para_comparar}: {contador}")
+    print(f"LA CANTIDAD DE NOTAS MAYORES QUE {numero_para_comparar}: {contador}")
 
 
 
 elif int(option) == 4:
     acumulador2 = 0
 
-    nota = input("Ingrese las notas separadas por coma: ").split(",")
+    nota = input("INGRESE LAS NOTAS SEPARADAS POR COMAS: ").split(",")
     notas = list(map(int, nota))
     for i in notas:
         acumulador2 += i
         promedio = acumulador2/len(notas)
-    print(f"Las notas son: {notas}")
-    print("El promedio es: ", promedio)
+    print(f"LAS NOTAS SON: {notas}")
+    print("EL PROMEDIO ES: ", promedio)
