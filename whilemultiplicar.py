a197eaaa433674a4dc8ba3f971c73d5c5a8c7c84
braian <braiancardona1245@gmail.com>
2025-05-02 06:58:35 -0500

    arre

--- whilemultiplicar.py
+++ whilemultiplicar.py
@@ -1,10 +1,7 @@
+num = int(input("ingrese un numero: "))
 i = 1
 
-
-num = int(input("ingresa un numero: \n"))
-while i in num:
-    if i:
-        print(f"{i} X {num} = ",i*num)
-    i=i+1
-
-    
+while i <= 100:
+        resultado = num * i
+        print(f"{num} x {i} = {resultado}")
+        i += 1
