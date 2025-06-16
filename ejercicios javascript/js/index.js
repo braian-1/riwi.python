032f4842ad1aa80bc4f3ad399db52ec6eec6f598
braian <braiancardona1245@gmail.com>
2025-06-16 13:24:06 -0500

    javascr

---
+++ ejercicios javascript/js/index.js
@@ -0,0 +1,119 @@
+//parte 1
+//nombre empresa
+let empresa = "google";
+console.log(empresa);
+
+//Definir cantidad
+let numerica = "cantidad";
+const cantidad = 150;
+console.log(cantidad);
+
+//Establecer precio
+let decimal = "precio";
+const precio = 29.99;
+console.log(precio);
+
+//activo o inactivo
+let activo = true;
+console.log("el usuario o sistema esta activo");
+inactivo = false;
+console.log("el usuario o sistema esta inactivo");
+
+//actualizar
+let puntos = 100;
+puntos = puntos + 20;
+console.log(puntos);
+
+//analista de datos 
+
+//parte 2
+//verificar codigo postal
+let postal = "05001";
+console.log("codigo postal:",postal);
+console.log("tipo de dato:",typeof postal);
+
+//coteo empleados
+let numeroEmpleados = 50;
+console.log(numeroEmpleados);
+console.log(typeof numeroEmpleados);
+
+//porcentaje
+let porcentajeIVA = 0.19;
+decimal = porcentajeIVA / 100;
+console.log(decimal);
+console.log(typeof decimal)
+
+//estado de disponibilidad
+let variable = false;
+console.log(variable);
+console.log(typeof variable);
+
+//clasificar informacion
+let animal = "perro";
+let numeroPatas = 4;
+let mamifero = true;
+console.log(animal);
+console.log(numeroPatas);
+console.log(mamifero);  
+
+//parte 3
+
+//calcular saldo
+let inicial = 500;
+let deposito = 200;
+resultado = (inicial + deposito);
+console.log(resultado);
+
+
+//salario
+let horastrabajo = 40;
+let tarifahora = 25.5;
+resultado1 = (horastrabajo * tarifahora);
+console.log("Salario bruto es:",resultado1);
+
+//ventas
+let ventasTotales = 12000;
+let vendedores = 4;
+resultado2 = (ventasTotales / vendedores);
+console.log("El promedio por vendedor es",resultado2);
+
+//duracion clases
+let duracionclases = 75;
+let bloqueclase = 60;
+resultado3 = (duracionclases - bloqueclase);
+console.log("los minutos restantes son",resultado3);
+
+
+//parte 4
+//ciudad y pais
+let ciudad = "medellin";
+let pais = "colombia";
+console.log("estoy en ",ciudad,",",pais);
+
+//telefono
+
+let telefono = "telefono";
+let numero = "3101234567";
+
+console.log("contacto completo:\n",telefono,":",numero);
+
+//descripcion de articulo
+let articulo = "libro";
+let genero = "ficcion";
+
+console.log("El articulo es",articulo,"de genero", genero);
+
+//fecha
+
+let dia = "lunes";
+let fecha = "16";
+
+console.log("hoy es",dia,fecha);
+
+//personal
+
+let accion = "aprender";
+let objeto = "programacion";
+console.log("Me gusta",accion,objeto);
+
+   
