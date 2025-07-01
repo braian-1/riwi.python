dcd4f0b502422a35a834cef8e2801400f50f53f7
braian <braiancardona1245@gmail.com>
2025-07-01 13:29:51 -0500

    java12

--- S2 M3 javascript/index.js
+++ S2 M3 javascript/index.js
@@ -1,37 +1,46 @@
 console.log("!Gestion de datos con objetos, Sets y Maps¡");
 
+//Definir el objeto productos
 const productos = {
-    1: { id: 1, nombre: "Guayos",precio: 1500},
-    2: { id: 2, nombre: "Uniformes",precio: 25},
-    3: { id: 3, nombre: "Medias",precio: 50}
+    1: { id: 1, nombre: "Carro",precio: 9500},
+    2: { id: 2, nombre: "Moto",precio: 3500},
+    3: { id: 3, nombre: "Bicicleta",precio: 1500}
 };
 
-console.log("Objeto productos:",productos);
+console.log("Objeto productos:", productos);
 
+//Crear un set con los nombres de los productos
 const setProductos = new Set(Object.values(productos).map(producto => producto.nombre));
 console.log("set de productos unicos:", setProductos);
 
+
+//Crear un map para agregar categorias a los productos 
 const mapProductos = new Map([
-    ["deportivo","guayos"],
-    ["deportivo","uniformes"],
-    ["deportivo","medias"]
-]);
+    ["Transporte","Carro"],
+    ["Transporte","Moto"],
+    ["Transporte","Bicicleta"]
+]); 
 
-console.log("Map de productos y categorias:",mapProductos);
+console.log("Map de productos y categorias:", mapProductos);
 
-for (const id in productos){
-    console.log(`producto ID: ${id}, detalles:`,productos[id]);
+
+//Recorrer el objeto producto
+for (const id in productos) {
+    console.log(`Producto ID: ${id}, Detalles:`, productos[id]);
 }
 
-for (const producto of setProductos){
-    console.log("producto unico:",producto);
+
+//Recorrer el set de productos
+for (const producto of setProductos) {
+    console.log("producto unico:", producto);
 }
 
-mapProductos.forEach((producto,categoria) => {
+//Recorrer el map de productos
+mapProductos.forEach((producto, categoria) => {
     console.log(`categoria: ${categoria}, producto: ${producto}`);
 });
 
 console.log("Prueba completa de gestion de datos:");
-console.log("Lista de productos (Objeto):",productos);
-console.log("Lista de productos unicos (set):",setProductos);
-console.log("Categorias y Productos (Map):",mapProductos);
+console.log("Lista de productos (Objeto):", productos);
+console.log("Lista de productos unicos (set):", setProductos);
+console.log("Categorias y Productos (Map):", mapProductos);
---
+++ index1.js
@@ -0,0 +1,144 @@
+//Codigo sincrono
+console.log("Inicio")
+
+for (let i = 0; i < 100000000; i++){ }
+
+console.log("Fin")
+
+//event looop (bucle de eventos)
+
+//Componentes del event loop
+//- call stack (pila de ejecucion)
+//- web APIs (APIs del navegador) o node.js: setTimeout()...
+//- task Queue (cola de tareas) y microtaskQueue
+
+//Flujo del event loop:
+// Callstak
+// Operaciones asincronas -> web APIs o node.js
+// Operacion termina -> la coloca en task Queue o microstask Queue
+// Si call task vacio -> Mueve tareas del microtask Queue o task Queue al call task
+
+//codigo asincrono
+//-- callbacks
+//el setTimeout simula asincronia
+console.log("Inicio")
+setTimeout(() => {
+    console.log("Esto se ejecuta")
+})
+console.log("Fin")
+
+//Problema: Callback Hell
+function step1(callback){
+    setTimeout(() => {
+        console.log("paso 1 completado")
+        callback()
+    }, 1000)
+}
+
+step1(() => {
+    console.log("Todos los pasos completados")
+})
+
+function step2(callback){
+    setTimeout(() => {
+        console.log("paso 2 completado")
+        callback()
+    }, 1000)
+}
+
+
+function step3(callback){
+    setTimeout(() => {
+        console.log("paso 3 completado")
+        callback()
+    }, 1000)
+}
+
+
+step1(() => {
+    step2(() => {
+        step3(() => {
+            console.log("Todos los pasos completados")
+        })
+    })
+})
+
+
+//Promesas
+
+const promise = new Promise((resolve, reject) => {
+    setTimeout(() => {
+        const ok = true
+        if (ok){
+            resolve("Operacion exitosa")
+        }else{
+            reject("Se ha producido un error")
+        }
+    }, 4000)
+})
+
+promise
+    .then(result => {
+        console.log(result)
+    })
+    .catch(error => {
+     console.log(error)
+    })    
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//Callbacks
+function processData(data, callback){
+    const result = sum(...data)
+    callback(result)
+}
+
+function processResult(element){
+    console.log(element)
+}
+
+function processResult2(element){
+    console.log(`mi resultado es ${element}`)
+}
+
+processData([1, 2, 3], processResult)
+processData([1, 2, 3], processResult2)
+processData([1, 2, 3], (element) => {
+    console.log(`mi resultado en la arrow function es ${element}`)
+})
+
+
+
