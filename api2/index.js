b527650b6719e9e30ec4fd9bad30f044073ca22a
braian <braiancardona1245@gmail.com>
2025-07-03 13:26:32 -0500

    cambios api

--- api2/index.js
+++ api2/index.js
@@ -1,32 +1,19 @@
-function buscarPokemon(){
-    const nombre = document.getElementById("nombrePokemon").value.toLowerCase().trim();
-    const resultado = document.getElementById("resultado");
+let buton = document.getElementById('button')
 
-    if(!nombre){
-        resultado.innerHTML = "<p>Por favor escribe el nombre de un pokemon.</p>";
-        return;
+buton.addEventListener('click',()=>{
+    let caja = document.getElementById('caja').value
+    let img = document.getElementById('img')
+    let p = document.getElementById('info')
+    let xhttp = new XMLHttpRequest()
+    xhttp.open("GET",`https://pokeapi.co/api/v2/pokemon/${caja}`)
+    xhttp.send()
+
+    xhttp.onreadystatechange = function (){
+        if(this.readyState==4 && this.status==200){
+            let datoPokemon=JSON.parse( this.responseText)
+            console.log(datoPokemon)
+            img.setAttribute("src",datoPokemon.sprites.front_default)
+            p.textContent=datoPokemon.name
+        }
     }
-
-    const url = `https://pokeapi.co/api/v2/pokemon/${nombre}`;
-
-    fetch(url)
-        .then(response => {
-            if(!resultado.ok){
-                throw new Error("Pokemon no encontrado");
-            }
-            return response.json();
-         })
-        .then(pokemon => {
-            resultado.innerHTML=`
-        <div class="pokemon">
-        <img src="${pokemon.sprites.front_default}" alt="${pokemon.name}">
-        <h2>${pokemon.name}</h2>
-        <p><strong>tipo:</strong>${pokemon.types.map(t => t.type.name).join(",")}</p>
-        <p><strong>Altura:</strong>${pokemon.height}</p>
-        <p><strong>Peso:</strong>${pokemon.weight}</p>
-        </div>`;
-    })
-    .catch(error =>{
-        resultado.innerHTML=`<p style="color:red;"> Pokemon no encontrado. Intenta con otro nombre.</p>`;
-    });
-}
+})
