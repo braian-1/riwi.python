b4ff1e0d6db6f7f9afe709327ebe538d6f07841a
braian <braiancardona1245@gmail.com>
2025-07-17 10:31:53 -0500

    new proje

---
+++ API Qr/script.js
@@ -0,0 +1,14 @@
+let input = document.querySelector('#input')
+let btnGenerateQR = document.querySelector('#generateQR')
+let imageQR = document.querySelector('#imageQR')
+
+function generateQR(){
+    if(input.value === ''){
+        alert('Please provide valid input')
+        return
+    }
+
+    imageQR.setAttribute('src', 'https://api.qrserver.com/v1/create-qr-code/?size=150x150&data=' + input.value)
+}
+
+btnGenerateQR.addEventListener('click', generateQR)
